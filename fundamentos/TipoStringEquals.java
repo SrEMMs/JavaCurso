fac6b4ceab6d6cabd8978b7aa0f8a63b5ccb3df6
Erick Melo <mmelo.erick@gmail.com>
2022-11-28 10:59:07 -0300

    Equals

---
+++ fundamentos/TipoStringEquals.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class TipoStringEquals{
+    public static void main(String[] args) {
+
+        System.out.println("2" == "2");
+        
+        String s1 = new String("2");
+        System.out.println("2" == s1);
+        System.out.println("2".equals(s1));
+
+        Scanner entrada = new Scanner(System.in);
+
+        String s2 = entrada.nextLine();
+        //.trim() retira os espaços em branco, o next() tambem faz essa operação
+        System.out.println("2"  == s2.trim());
+        System.out.println("2".equals(s2));
+        System.out.println("2".equals(s2.trim()));
+
+        entrada.close();
+        
+    }
+}
